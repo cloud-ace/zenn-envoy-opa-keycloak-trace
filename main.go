5ce7da127010e81bd8bfaff58cde743a64ecbfac
makoto.mase <makoto.mase@gmail.com>
2023-12-11 16:12:19 +0900

    add

--- main.go
+++ main.go
@@ -267,7 +267,6 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/api/v1/sleep", http.HandlerFunc(h.sleep))
 	mux.Handle("/api/v1/chain", http.HandlerFunc(h.sleepAndCall))
-	//mux.Handle("/metrics", promhttp.Handler())
 	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`{"status": "OK"}`))
 	}))
